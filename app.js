427151b4edbf245632a8bf370ad7cb26afeeeee9
christiana <tiwaslimzy@gmail.com>
2019-09-11 06:59:37 -0700

    JavaScriptAssignmentLinked

---
+++ app.js
@@ -0,0 +1,14 @@
+// function School(){
+//     var div = document.getElementById("one");
+//     var p_one =document.createElement("p");
+//     var text = "Simple Text";
+//     p_one.innerHTML = text;
+//     div.appendChild(p_one);
+// }
+
+
+var div = document.getElementById("one");
+var p_one = document.createElement("p");
+var text = "Simple Text";
+p_one.innerHTML = text;
+div.appendChild(p_one);
